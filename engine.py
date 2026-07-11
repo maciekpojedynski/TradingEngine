557348bf160cf6d7d8217aa6f9f08ff84390d2aa
Maciek <pojedynskimaciej@gmail.com>
2026-07-12 00:13:01 +0200

    perf: optimize algoritm to O(n)

--- engine.py
+++ engine.py
@@ -3,25 +3,37 @@ class TradingEngine:
         self.prices = price
 
     def max_revenue_v1(self) -> int:
+        "Brutal-Force version. O(n^2) time, O(1) memory"
         max_revenue = 0
-
-        #slow pointer
         for x in range(0, len(self.prices)):
-            #fast pointer
-            for y in range(x+1, len(self.prices)):
-                 revenue = self.prices[y] - self.prices[x]
-                 if max_revenue < revenue:
+            for y in range(x + 1, len(self.prices)):
+                revenue = self.prices[y] - self.prices[x]
+                if max_revenue < revenue:
                     max_revenue = revenue
-                 else:
-                    pass
-        
+        return max_revenue
+
+    def max_revenue_v2(self) -> int:
+        "Sliding window: O(n) time, O(1) memory"
+        max_revenue = 0
+        min_price = float('inf')
+
+        for index in range(0, len(self.prices)):
+            if self.prices[index] < min_price:
+                min_price = self.prices[index]
+            else:
+                revenue = self.prices[index] - min_price
+                if revenue > max_revenue:
+                    max_revenue = revenue
+
         return max_revenue
 
 if __name__ == '__main__':
     test_v1 = [105, 102, 103, 101, 104, 110, 100, 108]
     engine = TradingEngine(test_v1)
-    result = engine.max_revenue_v1()
-    print(result)
+    result_1 = engine.max_revenue_v1()
+    result_2 = engine.max_revenue_v2()
+    print(f"result_v1: {result_1}")
+    print(f"result_v1: {result_2}")
 
 
 

e20a069dfc46e8a01049bcee517887bde794010b
Maciek <pojedynskimaciej@gmail.com>
2026-07-16 21:46:55 +0200

    feat: integrate generator with google cloud storage bronze bucket

--- generator.py
+++ generator.py
@@ -2,6 +2,9 @@ from datetime import datetime
 import random
 import logging
 import time
+from google.cloud import storage
+import json
+
 
 logging.basicConfig(
     level = logging.INFO,
@@ -28,18 +31,53 @@ def generate_trade_event():
 
     return json_file
 
+def upload_to_gcs(bucket_name: str, data: dict, destination_blob_name: str):
+    """
+    Wysyla slownik jako plik json do gcs
+
+    Args:
+        bucket_name (str)
+        data (dict)
+        destination_blob_name (str)
+    """
+    storage_client = storage.Client.from_service_account_json('gcp-keys.json')
+    
+    bucket = storage_client.bucket(bucket_name)
+    
+    blob = bucket.blob(destination_blob_name)
+    
+    json_data_string = json.dumps(data)
+    
+    blob.upload_from_string(
+          json_data_string
+        , content_type = 'application/json'
+        , timeout = 60
+    )
+
+
 
     
 
 if __name__ == '__main__':
-    logging.info("Rozpoczynam generowanie informacji o akcjach...")
+    logging.info("Rozpoczynam generowanie informacji o akcjach i wyslanie do GCS")
+
+    bucket_name = 'trading-bronze-2026'
 
     try:
         while True:
-            logging.info(f"Obecne wartosci: \n {generate_trade_event()}")
 
-            time.sleep(2)
+            event = generate_trade_event()
+            clean_time = event["timestamp"].replace("-", "").replace(":", "").replace(" ", "_")
+            file_name = f"{event['ticker']}_{clean_time}.json"
+
+            logging.info(f"Generuję dane: {event}")
+            
+            upload_to_gcs(bucket_name, event, file_name)
+
+            logging.info(f"Pomyślnie wysłano plik {file_name} do GCS.")
         
     except KeyboardInterrupt:
 
         logging.info("Zakonczenie programu")
+
+        time.sleep(2)
